4798f48f984389978b52d6ceafabb17f5c4031a9
Kyle Miller <kylemiller995@gmail.com>
2017-10-16 09:09:45 +0100

    add stuff

--- specs/heroTest.js
+++ specs/heroTest.js
@@ -2,10 +2,13 @@ var assert = require('assert');
 var Hero = require('../hero.js');
 var Food = require('../food.js');
 var Task = require('../task.js');
+var Race = require('../race.js');
+var Armour = require('../armour.js');
 
 describe('Hero', function () {
 
   var hero1;
+  var race1;
   var food1;
   var task1;
   var task2;
@@ -13,20 +16,24 @@ describe('Hero', function () {
   var task4;
   var task5;
   var task6;
+  var armour1;
 
   beforeEach(function () {
-    hero1 = new Hero("xyz", "cheese");
+    humanRace = new Race("human", 100);
+    hero1 = new Hero("xyz", "cheese", humanRace);
     food1 = new Food("apple", 10);
-    task1 = new Task("high", "medium", 25)
-    task2 = new Task("medium", "high", 32)
-    task3 = new Task("medium", "medium", 52)
-    task4 = new Task("medium", "high", 20)
-    task5 = new Task("low", "low", 2)
-    task6 = new Task("medium", "low", 50)
+    task1 = new Task("high", "medium", 25);
+    task2 = new Task("medium", "high", 32);
+    task3 = new Task("medium", "medium", 52);
+    task4 = new Task("medium", "high", 20);
+    task5 = new Task("low", "low", 2);
+    task6 = new Task("medium", "low", 50);
+    armour1 = new Armour(10, 10, "legs");
+
   });
 
   it("should be able to talk", function () {
-    assert.strictEqual(hero1.talk(), "xyz")
+    assert.strictEqual(hero1.talk(), "xyz");
   });
 
   it("should be able to add to quests", function () {
@@ -78,5 +85,14 @@ describe('Hero', function () {
     task3.finishQuest(task3);
     assert.deepEqual(hero1.filterQuests(false), [task1, task2])
   })
+
+  it("should be able to pick up item", function () {
+    hero1.pickUp(armour1);
+    assert.strictEqual(hero1.inventory.length, 1);
+  })
+
+  it("should be able to equip item"), function () {
+
+  }
 
 })
